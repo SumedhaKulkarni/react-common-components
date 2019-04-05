cffcf49813e17d98f92ca8083ab2095a89601b53
Nilesh Pawar <nilpawar@deloitte.com>
2019-04-05 12:10:51 +0530

    test(component): adding missing tests

--- library/src/core/atoms/Badge/Badge.test.js
+++ library/src/core/atoms/Badge/Badge.test.js
@@ -11,9 +11,19 @@ const props = {
   classname: 'my-badge',
   effect: 'bounce',
   shape: 'circle',
-  count: '99+'
+  count: '99+',
 };
 
 it('should render the accordion component with props', () => {
   render(<BasicBadge {...props} />);
+});
+
+it('should add class bounce for bounce effect', () => {
+  const { getByText } = render(<BasicBadge {...props} />);
+  expect(getByText('99+')).toHaveClass('bounce');
+});
+
+it('should add class circle for circular shape', () => {
+  const { getByText } = render(<BasicBadge {...props} />);
+  expect(getByText('99+')).toHaveClass('circle');
 });
