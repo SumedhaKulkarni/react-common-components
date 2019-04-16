722d2e8089016b3b11d94965be3a7431336fd778
settabhishek <absett@deloitte.com>
2019-04-16 16:55:06 +0530

    test(component): checkbox test cases added
    checkbox test cases added

--- src/core/atoms/Checkbox/Checkbox.js
+++ src/core/atoms/Checkbox/Checkbox.js
@@ -1,70 +1,70 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
 import {
-    node, string, bool, func,
+  node, string, bool, func,
 } from 'prop-types';
 import './Checkbox.css';
 
 const id = `check${Math.floor(100000 + Math.random() * 900000)}`;
 
 function Checkbox({
-    classname,
-    displayTextClassname,
-    appearance,
-    value,
-    displayText,
-    children,
-    isChecked,
-    disabled,
-    handleChange = () => { },
-    ...props
+  classname,
+  displayTextClassname,
+  appearance,
+  value,
+  displayText,
+  children,
+  isChecked,
+  disabled,
+  handleChange = () => { },
+  ...props
 }) {
-    const [checked, setChecked] = useState(isChecked || false);
+  const [checked, setChecked] = useState(isChecked || false);
 
-    const handleCheckboxChange = (e) => {
-        setChecked(e.target.checked);
-        handleChange(e.target.checked, value);
-    };
+  const handleCheckboxChange = (e) => {
+    setChecked(e.target.checked);
+    handleChange(e.target.checked, value);
+  };
 
-    return (
-        <div className={clsx(classname, appearance, 'checkbox-component')}>
-            <label htmlFor={id} className={clsx(displayTextClassname, 'checkbox-text', 'checkbox-container')}>{displayText}
-                <input
-                    id={id}
-                    type="checkbox"
-                    value={value}
-                    checked={checked}
-                    disabled={disabled}
-                    onChange={handleCheckboxChange}
-                    data-testid="checkbox"
-                    {...props}
-                />
-                <span className="checkmark" />
-            </label>
-        </div>
-    );
+  return (
+    <div className={clsx(classname, appearance, 'checkbox-component')}>
+      <label data-testid="checkbox-text" htmlFor={id} className={clsx(displayTextClassname, 'checkbox-text', 'checkbox-container')}>{displayText}
+        <input
+          id={id}
+          type="checkbox"
+          value={value}
+          checked={checked}
+          disabled={disabled}
+          onChange={handleCheckboxChange}
+          data-testid="checkbox"
+          {...props}
+        />
+        <span className="checkmark" />
+      </label>
+    </div>
+  );
 }
 
 Checkbox.propTypes = {
-    children: node.isRequired,
-    disabled: bool,
-    value: string,
-    displayText: string,
-    isChecked: bool,
-    handleChange: func.isRequired,
-    classname: string,
-    displayTextClassname: string,
-    appearance: string,
+  children: node,
+  disabled: bool,
+  value: string,
+  displayText: string,
+  isChecked: bool,
+  handleChange: func.isRequired,
+  classname: string,
+  displayTextClassname: string,
+  appearance: string,
 };
 
 Checkbox.defaultProps = {
-    disabled: false,
-    classname: '',
-    value: '',
-    isChecked: false,
-    displayTextClassname: '',
-    appearance: '',
-    displayText: '',
+  disabled: false,
+  classname: '',
+  value: '',
+  isChecked: false,
+  displayTextClassname: '',
+  appearance: '',
+  displayText: '',
 };
 
 export default Checkbox;
