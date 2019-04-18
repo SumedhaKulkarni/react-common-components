4db195f06e5ce6a94210c4cd3150616819a57fde
swapnil dubal <sdubal@deloitte.com>
2019-04-18 14:56:02 +0530

    build(package json): updated scripts in package json for release

---
+++ src/core/index.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+/* atoms */
+import Accordion from './atoms/Accordion/Accordion';
+import Badge from './atoms/Badge/Badge';
+import Button from './atoms/Button/Button';
+import Checkbox from './atoms/Checkbox/Checkbox';
+import Label from './atoms/Label/Label';
+import Link from './atoms/Link/Link';
+import Modal from './atoms/Modal/Modal';
+import RadioOption from './atoms/RadioOption/RadioOption';
+import Switch from './atoms/Switch/Switch';
+import Tab from './atoms/Tab/Tab';
+import Textbox from './atoms/Textbox/Textbox';
+
+/* molecules */
+import Autosuggest from './molecules/Autosuggest/Autosuggest';
+import RadioGroup from './molecules/RadioGroup/RadioGroup';
+
+export {
+  Accordion,
+  Badge,
+  Button,
+  Checkbox,
+  Label,
+  Link,
+  Modal,
+  RadioOption,
+  Switch,
+  Tab,
+  Textbox,
+  Autosuggest,
+  RadioGroup,
+};
