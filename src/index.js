013670087f32e04bafeb8f13905c1588b83490d1
swapnil dubal <sdubal@deloitte.com>
2019-04-18 15:12:43 +0530

    build(package json): updated the build and npm publish script. Changed the folder strcture a bit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Button from './core/atoms/Button/Button';
+
+ReactDOM.render(<Button>I am button </Button>, document.getElementById('root'));
+
+
+/* atoms */
+// import Accordion from './core/atoms/Accordion/Accordion';
+// import Badge from './core/atoms/Badge/Badge';
+// import Button from './core/atoms/Button/Button';
+// import Checkbox from './core/atoms/Checkbox/Checkbox';
+// import Label from './core/atoms/Label/Label';
+// import Link from './core/atoms/Link/Link';
+// import Modal from './core/atoms/Modal/Modal';
+// import RadioOption from './core/atoms/RadioOption/RadioOption';
+// import Switch from './core/atoms/Switch/Switch';
+// import Tab from './core/atoms/Tab/Tab';
+// import Textbox from './core/atoms/Textbox/Textbox';
+
+/* molecules */
+// import Autosuggest from './core/molecules/Autosuggest/Autosuggest';
+// import RadioGroup from './core/molecules/RadioGroup/RadioGroup';
+
+// export {
+//   Accordion,
+//   Badge,
+//   Button,
+//   Checkbox,
+//   Label,
+//   Link,
+//   Modal,
+//   RadioOption,
+//   Switch,
+//   Tab,
+//   Textbox,
+//   Autosuggest,
+//   RadioGroup,
+// };
