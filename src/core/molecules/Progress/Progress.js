9a26294c7b0dd4a771854219e0562e69dcfa5859
Sharma, Ritu (US - Mumbai) <ritsharma@deloitte.com>
2019-05-03 11:46:40 +0530

    test(Progress component): Added test scripts
    BREAKING CHANGE: Test scripts

--- src/core/molecules/Progress/Progress.js
+++ src/core/molecules/Progress/Progress.js
@@ -8,12 +8,10 @@ import {
   number,
 } from 'prop-types';
 import './Progress.css';
-import Label from '../../atoms/Label/Label';
 
 function Progress({
   children,
   classname,
-  message,
   percent,
   type,
   messageInside,
@@ -30,7 +28,7 @@ function Progress({
     circumference = 2 * Math.PI * radius;
   }
   return (
-    <div className={clsx('progress', classname, { 'progress--cicle': type === 'circle' })} {...props}>
+    <div data-testid="progress" className={clsx('progress', classname, { 'progress--cicle': type === 'circle' })} {...props}>
       {
         type === 'bar'
           ? (
@@ -38,9 +36,11 @@ function Progress({
               <div
                 className="progress__bar__line"
                 style={{ height: `${strokeSize}px` }}
+                data-testid="progress-bar"
               >
                 <div
                   className="progress__bar__line__fill"
+                  data-testid="progress-bar-line-fill"
                   style={Object.assign({},
                     percent > 0
                     && { width: `${percent}%`, backgroundColor: fill, border: `solid 1px ${fill}` })}
@@ -48,18 +48,33 @@ function Progress({
               </div>
               <div
                 className={clsx('progress__bar__message', { 'progress__bar__message--inline': messageInside })}
+                data-testid="progress-message"
                 style={Object.assign({},
                   messageInside && (percent < 5) && { left: 0 },
                   messageInside && (percent >= 5) && { right: `${100 - percent}%` })}
               >
-                <Label htmlFor={`${percent}%`}>{ message || `${percent}%` }</Label>
+                {children || `${percent}%`}
               </div>
             </div>
           )
           : (
             <div className="progress__circle">
-              <svg className="progress__circle__svg" width={width} height={width} viewBox={`0 0 ${width} ${width}`}>
-                <circle className="progress-meter" cx={width / 2} cy={width / 2} r={radius} fill="none" stroke="#d3d3d3" strokeWidth={strokeSize} />
+              <svg
+                className="progress__circle__svg"
+                width={width}
+                height={width}
+                viewBox={`0 0 ${width} ${width}`}
+                data-testid="progress-circle"
+              >
+                <circle
+                  className="progress-meter"
+                  cx={width / 2}
+                  cy={width / 2}
+                  r={radius}
+                  fill="none"
+                  stroke="#d3d3d3"
+                  strokeWidth={strokeSize}
+                />
                 <circle
                   className="progress-value"
                   cx={width / 2}
@@ -70,10 +85,15 @@ function Progress({
                   strokeWidth={strokeSize}
                   strokeDasharray={circumference}
                   strokeDashoffset={circumference * (100 - percent) / 100}
+                  data-testid="progress-circle-fill"
                 />
               </svg>
-              <div className="progress__circle__message" style={{ padding: `${strokeSize}px` }}>
-                <Label htmlFor={`${percent}%`}>{ message || `${percent}%` }</Label>
+              <div
+                className="progress__circle__message"
+                style={{ padding: `${strokeSize}px` }}
+                data-testid="progress-message"
+              >
+                {children || `${percent}%`}
               </div>
             </div>
           )
@@ -85,7 +105,6 @@ function Progress({
 Progress.propTypes = {
   children: node,
   classname: string,
-  message: string,
   percent: number.isRequired,
   type: oneOf(['bar', 'circle']),
   messageInside: bool,
@@ -97,7 +116,6 @@ Progress.propTypes = {
 Progress.defaultProps = {
   children: null,
   classname: '',
-  message: '',
   type: 'bar',
   messageInside: false,
   strokeSize: 20,
